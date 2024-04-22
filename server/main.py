fd8f86fbeea82c1ede990199d55a337e0b664359
Irfan Rasheed <irfanrasheed273@gmail.com>
2024-04-23 01:09:59 +0530

    Backend update

--- server/main.py
+++ server/main.py
@@ -1,9 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
 client = MongoClient('mongodb+srv://irfanrasheedkc:gTo5RnpsY7mpL2BZ@cluster0.mznznpy.mongodb.net/?retryWrites=true&w=majority')
-db = client['Synerlink']
+db = client['synerlink']
 
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -11,6 +11,29 @@ def signup():
     # Store the details to MongoDB
     db.users.insert_one(data)
     return 'User details stored successfully'
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = db.users.find_one({'username': data['username']})
+    if not user or user['password'] != data['password']:
+        return jsonify({'error': 'Invalid username or password'}), 401
+    return jsonify({'message': 'Logged in successfully'}), 200
+
+@app.route('/create_project', methods=['POST'])
+def create_project():
+    data = request.get_json()
+    # Store the project details to MongoDB
+    db.projects.insert_one(data)
+    return 'Project details stored successfully'
+
+@app.route('/fetch_projects', methods=['GET'])
+def fetch_projects():
+        projects = db.projects.find()
+    project_list = []
+    for project in projects:
+            project_list.append(project)
+    return jsonify(project_list)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
