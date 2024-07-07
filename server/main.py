ae87ad382bc126ab198e638ad4e903a6c1a9b66b
Irfan Rasheed <irfanrasheed273@gmail.com>
2024-07-07 10:04:51 +0530

    Login , signup done

--- server/main.py
+++ server/main.py
@@ -1,24 +1,35 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 client = MongoClient('mongodb+srv://irfanrasheedkc:gTo5RnpsY7mpL2BZ@cluster0.mznznpy.mongodb.net/?retryWrites=true&w=majority')
-db = client['synerlink']
+db = client['myproject1']
 
 @app.route('/signup', methods=['POST'])
 def signup():
     data = request.get_json()
-    # Store the details to MongoDB
+    print(data)
+    # Extract email from the data
+    email = data.get('email')
+    
+    # Check if email already exists
+    if db.users.find_one({'email': email}):
+        # If email exists, return an error message
+        return jsonify({'signup': 'False'}), 200  
+    
+    # If email does not exist, insert the new user
     db.users.insert_one(data)
-    return 'User details stored successfully'
+    return jsonify({'signup': 'True'})
 
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    user = db.users.find_one({'username': data['username']})
+    user = db.users.find_one({'email': data['email']})
     if not user or user['password'] != data['password']:
-        return jsonify({'error': 'Invalid username or password'}), 401
-    return jsonify({'message': 'Logged in successfully'}), 200
+        return jsonify({'login': 'False'}), 200
+    return jsonify({'login': 'True'}), 200
 
 @app.route('/create_project', methods=['POST'])
 def create_project():
